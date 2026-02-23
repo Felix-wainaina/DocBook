85d48ea5b384b6028bdbd11de8b49fb3b9840cff
Liminal9000 <nkagochi@gmail.com>
2026-02-23 13:17:49 +0300

    Implemented Snackbar feedback for successful booking

--- app/src/main/java/com/example/docbook/DateFormatter.java
+++ app/src/main/java/com/example/docbook/DateFormatter.java
@@ -7,8 +7,6 @@ import java.util.Locale;
 
 public class DateUtils {
     public static String formatBookingDate(String rawDate) {
-        // Example: Converts "12-05" to "Friday, 12th May"
-        // This logic assumes 2026 based on the current project year
         try {
             SimpleDateFormat inputFormat = new SimpleDateFormat("dd-MM", Locale.getDefault());
             SimpleDateFormat outputFormat = new SimpleDateFormat("EEEE, d MMM", Locale.getDefault());
